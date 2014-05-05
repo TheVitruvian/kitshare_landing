49f0c896b4202d63f8bd3a62e280c9755edb223b
TheVitruvian <Alexham100@aol.com>
2014-05-05 15:10:29 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KitshareLanding::Application.config.secret_token = '944d88e37eb9be4c33851f1ed6b9c44d5bf3edca624637cfdd62e737d61a3e0566093f8a8ac51e01d909f612fff459df0191f75afba6f4c17bd41bc37399c93c'
